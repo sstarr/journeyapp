4e8980100f92bf79093bc181cfc8ca3317084115
Jordan Elver <jordan.elver@gmail.com>
2014-09-20 13:54:27 +0100

    Geocode journey postcodes as well

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,4 +1,11 @@
+require 'geocode_journey_postcodes'
+
 class Journey < ActiveRecord::Base
+  include GeocodeJourneyPostcodes
+
   belongs_to :user
   belongs_to :transport
+
+  validates :start_postcode, presence: true
+  validates :end_postcode,   presence: true
 end
